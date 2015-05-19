8cb1387a526a53d936530c56f3c37652c4cf22c2
Frank Bergmann <frank.bergmann@project-open.com>
2015-05-19 15:59:14 +0000

    - Helpdesk:   Fixed customer_contact_id_ajax

--- sql/postgresql/intranet-helpdesk-create.sql
+++ sql/postgresql/intranet-helpdesk-create.sql
@@ -1717,6 +1717,70 @@ SELECT im_dynfield_widget__new (
 
 
 
+
+SELECT im_dynfield_widget__new (
+	null, 'im_dynfield_widget', now(), 0, '0.0.0.0', null,
+	'customer_contact_select_ajax', 'Customer Contact Select AJAX', 'Customer Contact Select AJAX',
+	10007, 'integer', 'generic_sql', 'integer',
+	'{custom {sql {
+		select	u.user_id,
+			im_name_from_user_id(u.user_id) as name
+		from	users u
+		where	u.user_id not in (
+				-- Exclude deleted or disabled users
+				select	m.member_id
+				from	group_member_map m,
+					membership_rels mr
+				where	m.group_id = acs__magic_object_id(''registered_users'') and
+					m.rel_id = mr.rel_id and
+					m.container_id = m.group_id and
+					mr.member_state != ''approved''
+			)
+		order by name
+	}}
+	after_html {
+		<script type="text/javascript">
+		function customerContactSelectOnChange() {
+		    var xmlHttp1;
+		    try { xmlHttp1=new XMLHttpRequest();	// Firefox, Opera 8.0+, Safari
+		    } catch (e) {
+			try { xmlHttp1=new ActiveXObject("Msxml2.XMLHTTP");	// Internet Explorer
+			} catch (e) {
+			    try { xmlHttp1=new ActiveXObject("Microsoft.XMLHTTP");
+			    } catch (e) {
+				alert("Your browser does not support AJAX!");
+				return false;
+			    }
+			}
+		    }
+		    xmlHttp1.onreadystatechange = function() {
+			if(xmlHttp1.readyState==4) {
+			    var divElement = document.getElementById(''customer_contact_div'');
+				divElement.innerHTML = this.responseText;
+			}
+		    }
+		    var customer_id = document.helpdesk_ticket.ticket_customer_contact_id.value;
+		    xmlHttp1.open("GET","/intranet/components/ajax-component-value?plugin_name=Customer%20Info&package_key=intranet-helpdesk&ticket_customer_contact_id=" + customer_id,true);
+		    xmlHttp1.send(null);
+		}
+		window.onload = function() {
+		    var dropdown = document.helpdesk_ticket.ticket_customer_contact_id;
+		    dropdown.onchange = customerContactSelectOnChange;
+
+		    var divElement = document.getElementById(''customer_contact_div'');
+		    if (divElement != null){
+			var div = document.helpdesk_ticket.ticket_customer_contact_id;
+			div.onchange = customerContactSelectOnChange;        
+			if (div.value != null) { customerContactSelectOnChange() }
+		}
+		}
+		</script>
+	}
+}'
+);
+
+
+
 SELECT im_dynfield_widget__new (
 	null, 'im_dynfield_widget', now(), 0, '0.0.0.0', null,
 	'ticket_assignees', 'Ticket Assignees', 'Ticket Assignees',
