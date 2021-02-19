27789b251ed9619edadbe31dac99c47d1b757deb
Project Open <info@project-open.com>
2021-02-19 18:00:50 +0100

    http -> httpd

--- www/request-sla.tcl
+++ www/request-sla.tcl
@@ -3,7 +3,7 @@
 # Copyright (C) 2003-2008 ]project-open[
 #
 # All rights reserved. Please check
-# http://www.project-open.com/license/ for details.
+# https://www.project-open.com/license/ for details.
 
 ad_page_contract {
     This page is called if a user is not a member of any SLA
